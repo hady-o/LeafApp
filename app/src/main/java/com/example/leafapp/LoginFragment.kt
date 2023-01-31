596ddf0bf5b4a51ce0cb07306304c7b5458711fb
Hadi Atef <76757642+hady-o@users.noreply.github.com>
2023-01-31 18:19:20 +0200

    google sign in v1

--- app/src/main/java/com/example/leafapp/LoginFragment.kt
+++ app/src/main/java/com/example/leafapp/LoginFragment.kt
@@ -1,24 +1,42 @@
 package com.example.leafapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.leafapp.databinding.FragmentLoginBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class LoginFragment : Fragment() {
-
-
+    private val LOGINCODE = 505
+    private lateinit var client: GoogleSignInClient
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val binding =  FragmentLoginBinding.inflate(layoutInflater)
+
+        val  options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        client = GoogleSignIn.getClient(requireActivity(),options)
+
+
 
 
         // buttons clicks
@@ -36,19 +54,45 @@ class LoginFragment : Fragment() {
         //google sign in button
         binding.googleSignInBtnId!!.setOnClickListener()
         {
-           var signInRequest = BeginSignInRequest.builder()
-                .setGoogleIdTokenRequestOptions(
-                    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.default_web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-                .build();
+            val intent = client.signInIntent
+            startActivityForResult(intent,LOGINCODE)
         }
+
+
+
 
 
         return binding.root
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==LOGINCODE){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener{task->
+                    if(task.isSuccessful){
+
+                        Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_homeFragment)
+
+                    }else{
+                        Toast.makeText(requireContext(),task.exception?.message,Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if(FirebaseAuth.getInstance().currentUser != null){
+            Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/leafapp/ProfileFragment.kt
+++ app/src/main/java/com/example/leafapp/ProfileFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.example.leafapp.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ProfileFragment : Fragment() {
@@ -17,9 +20,21 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding =  FragmentProfileBinding.inflate(layoutInflater)
         //set slogan colored text
-       // val name = getColoredSpanned("To gather we can make", "#224637")
         val surName = getColoredSpanned("deference", "#6BDBAB")
         binding.slogan.text = Html.fromHtml("To gather we can<br>make"+" "+surName)
+        //user data
+        var user = FirebaseAuth.getInstance().currentUser
+        binding.nameEditText.setText(user!!.displayName.toString())
+        binding.emailEditText.setText(user!!.email.toString())
+        Glide.with(requireContext())
+            .load(user.photoUrl)
+            .into(binding.userImage)
+
+        binding.cancelBtnId.setOnClickListener()
+        {
+            FirebaseAuth.getInstance().signOut()
+            Navigation.findNavController(binding.root).navigate(R.id.action_profileFragment_to_loginFragment)
+        }
         return binding.root
     }
     private fun getColoredSpanned(text: String, color: String): String? {
