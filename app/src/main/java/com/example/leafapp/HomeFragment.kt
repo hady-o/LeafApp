f1f1ad4f2067013250719ca00c905cbabec64a58
Hadi Atef <76757642+hady-o@users.noreply.github.com>
2023-02-02 04:04:03 +0200

    Now we can access camera

--- app/src/main/java/com/example/leafapp/HomeFragment.kt
+++ app/src/main/java/com/example/leafapp/HomeFragment.kt
@@ -1,27 +1,36 @@
 package com.example.leafapp
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.graphics.createBitmap
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.example.leafapp.databinding.FragmentHomeBinding
 import com.example.leafapp.homefragments.*
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 
 class HomeFragment : Fragment() {
-
+    public lateinit var binding: FragmentHomeBinding
+    private val CAMERA_REQUEST = 1888
+    private val MY_CAMERA_PERMISSION_CODE = 100
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding =  FragmentHomeBinding.inflate(layoutInflater)
+        binding =  FragmentHomeBinding.inflate(layoutInflater)
        // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         var tabLayoutAdapter = TabLayoutAdapter(parentFragmentManager)
         tabLayoutAdapter.addFragment(AllFragment(), "All")
@@ -46,6 +55,40 @@ class HomeFragment : Fragment() {
                 .into(binding.userImage)
         }
 
+        // cam btn
+        binding.camBtn.setOnClickListener()
+        {
+            if (checkSelfPermission(requireContext(),Manifest.permission.CAMERA) !== PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), MY_CAMERA_PERMISSION_CODE)
+            } else {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            }
+        }
+
+
         return binding.root
     }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), "camera permission granted", Toast.LENGTH_LONG).show()
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            } else {
+                Toast.makeText(requireContext(), "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+           val photo = data!!.extras!!["data"] as Bitmap?
+       }
+   }
 }
+
