d84c428d372978fd0791fd1a63fc87fd7687b67f
Hadi Ehab <hadiehab17@gmail.com>
2023-04-27 00:17:31 +0200

    Translated Overview

--- app/src/main/java/com/example/leafapp/DiseasesData.kt
+++ app/src/main/java/com/example/leafapp/DiseasesData.kt
@@ -116,7 +116,7 @@ object DiseasesData {
                     cause = it[4],
                     howItStared = it[5],
                     tips = it[6],
-                    code = it[6]
+                    code = it[7]
                 )
                 lookUp[it[0]] = tmp
                 if(it[0].equals(Constants.ID,true).not())
